2c8f9c0fd5ff05e2acb0e4aef25bfc1e7d449d01
FatimaHarunaImam <101684307+FatimaHarunaImam@users.noreply.github.com>
2025-10-12 00:35:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 
-  import { defineConfig } from 'vite';
-  import react from '@vitejs/plugin-react-swc';
-  import path from 'path';
+  import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-  export default defineConfig({
-    plugins: [react()],
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'build'
+  }
+})
+
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
